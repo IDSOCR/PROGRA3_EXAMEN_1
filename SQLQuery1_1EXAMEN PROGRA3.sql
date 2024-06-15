133edcc1cd3ea87d6adaa751b53c9d85319be8e3
Ivar David Solera Quesada <103919789+IDSOCR@users.noreply.github.com>
2024-06-14 18:29:22 -0600

    Add files via upload

---
+++ SQLQuery1_1EXAMEN PROGRA3.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE SistemaGestionReciclaje;
+
+-- para usar la base de datos
+USE SistemaGestionReciclaje;
+GO
+
+-- tabla Usuario
+CREATE TABLE Usuario (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    puntos INT DEFAULT 0
+);
+GO
+
+-- tabla MaterialReciclable
+CREATE TABLE MaterialReciclable (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    tipo VARCHAR(50) NOT NULL,
+    descripcion TEXT
+);
+GO
+
+-- tabla RegistroReciclaje
+CREATE TABLE RegistroReciclaje (
+    id INT PRIMARY KEY IDENTITY(1,1),
+    usuario_id INT,
+    material_id INT,
+    cantidad FLOAT NOT NULL,
+    fecha DATE NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES Usuario(id),
+    FOREIGN KEY (material_id) REFERENCES MaterialReciclable(id)
+);
